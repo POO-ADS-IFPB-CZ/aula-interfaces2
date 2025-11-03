19a449737321c810e209a8ae87767cedcfc74f42
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-11-03 07:59:07 -0300

    Testando as classes

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Voavel voavel1 = new Aviao("Airbus", "A320",
+                2010);
+        Voavel voavel2 = new Passaro("Pardal");
+
+        voavel1.voar();
+        voavel2.voar();
+
+
+    }
+
+}
